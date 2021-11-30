3c523f852f7454838eaa1751822a38f8bf0f3074
Jamil Nawaz <jamilnawaz88@gmail.com>
2021-11-30 21:42:44 +0000

    SQUAD-57: building functionality against acceptance criteria in SQUAD-57

--- __tests__/unit/handlers/game/post-game.test.js
+++ __tests__/unit/handlers/game/post-game.test.js
@@ -4,19 +4,120 @@ const lambda = require('../../../../src/handlers/game/post-game');
 
 var event, context, callback;
 
-describe('Test postGameHandler', () => {
+jest.mock('mysql', () => ({
+    state: 'disconnected',
+    createConnection: () => {},
+    connect: jest.fn().mockImplementation((callback) => callback()),
+    format: jest.fn(),
+    query: jest.fn().mockImplementation((query, callback) => callback(null, 
+        [{ 
+            device_id: 'device_id', device_make: 'device_make', device_model: 'device_model', ios_push_notification_token: 'ios_push', android_push_notification_token: 'android_push'
+        }]
+        )),
+        end: jest.fn()
+    }));
+    
+    describe('Test postGameHandler', () => {
+        
+        beforeEach( () => jest.resetModules() );
+        
+        it('should not accept the PUT http method', async done => {
+            event = {
+                httpMethod: 'PUT'
+            };
+            
+            const result = await lambda.postGameHandler(event, context, callback, mysql);
+            const expectedResult = {"message": "Bad request", "reason": "postGameHandler only accepts POST method, you tried: PUT", "statusCode": 400}
+            
+            expect(result).toEqual(expectedResult);
+            done();
+        });
+        
+        it('should not accept the GET http method', async done => {
+            event = {
+                httpMethod: 'GET'
+            };
+            
+            const result = await lambda.postGameHandler(event, context, callback, mysql);
+            const expectedResult = {"message": "Bad request", "reason": "postGameHandler only accepts POST method, you tried: GET", "statusCode": 400}
+            
+            expect(result).toEqual(expectedResult);
+            done();
+        });
+        
+        it('should create and insert a game and the invited players', async done => {
+            
+            event = {
+                httpMethod: 'POST',
+                body: {
+                    game: {
+                        game_id: 'game_id',
+                        venue: 'venue',
+                        location: 'location',
+                        date_created: 'date_created',
+                        date_modified: 'date_modified',
+                        organising_player: 'organising_player'
+                    },
+                    invitedPlayers: [
+                        {
+                            organised_game_id: 'organised_game_id',
+                            response_id: 'response_id',
+                            date_responded: 'date_responded',
+                            can_play: 'can_play',
+                            date_modified: 'date_modified',
+                            user_id: 'user_id',
+                            mobile_number: 'mobile_number 0',
+                            email_address: 'email_address 0'
+                        },
+                        {
+                            organised_game_id: 'organised_game_id',
+                            response_id: 'response_id',
+                            date_responded: 'date_responded',
+                            can_play: 'can_play',
+                            date_modified: 'date_modified',
+                            user_id: 'user_id',
+                            mobile_number: 'mobile_number 1',
+                            email_address: 'email_address 1'
+                        },
+                    ]
+                }
+            }
 
-    beforeEach( () => jest.resetModules() );
+            const result = await lambda.postGameHandler(event, context, callback, mysql);
 
-    it('should not accept the PUT http method', async done => {
-        event = {
-            httpMethod: 'PUT'
-        };
+            const expectedResult = {
+                statusCode: 201,
+                body: {
+                    results: {
+                        game: {
+                            game_id: 'game_id',
+                            venue: 'venue',
+                            location: 'location',
+                            date_created: 'date_created',
+                            date_modified: 'date_modified',
+                            organising_player: 'organising_player'
+                        },
+                        invitedPlayers: [
+                            {
+                                organised_game_id: 'organised_game_id',
+                                response_id: 'response_id',
+                                date_responded: 'date_responded',
+                                can_play: 'can_play',
+                                date_modified: 'date_modified',
+                                user_id: 'user_id'
+                            },
+                            {
+                                organised_game_id: 'organised_game_id',
+                                response_id: 'response_id',
+                                date_responded: 'date_responded',
+                                can_play: 'can_play',
+                                date_modified: 'date_modified',
+                                user_id: 'user_id'
+                            },
+                        ]
+                    }
+                }
+            }
 
-        const result = await lambda.postGameHandler(event, context, callback, mysql);
-        const expectedResult = {"message": "Bad request", "reason": "postGameHandler only accepts POST method, you tried: PUT", "statusCode": 400}
-
-        expect(result).toEqual(expectedResult);
-        done();
+        });
     });
-});
--- src/handlers/game/post-game.js
+++ src/handlers/game/post-game.js
@@ -1,5 +1,6 @@
-const { HttpRequest } = require('aws-sdk');
 const mysql = require('mysql');
+const { from, of } = require('rxjs');
+const { filter } = require('rxjs/operators');
 
 exports.postGameHandler = async(event, context, callback, connection) => {
 
@@ -40,4 +41,46 @@ exports.postGameHandler = async(event, context, callback, connection) => {
         badRequest.reason = exception.message;
         return badRequest;
     }
+
+    try {
+        if (connection.state === 'disconnected') {
+            await new Promise((resolve, reject) => {
+                connection.connect(function (err) {
+                    if (err) {
+                        throw new Error('Failed to connect');
+                    }
+                    resolve();
+                });;
+            });
+
+            try {
+                var allMobileNumbers = [];
+                // // const playersObservable = new Observable()
+                // const requestBody = of(event.body);
+                // requestBody.pipe(filter(game => game.invitedPlayers)).subscribe((game) => {
+                //     allMobileNumbers.push(game.invitedPlayers.mobile_number);
+                // });
+
+                // Determine which users are registered
+                var nonRegisteredUsersSql = "SELECT * FROM User WHERE mobile_number in";
+                // Create ghost record in the user table for non registered users
+
+                // Invite non registered users
+
+                // Setup game
+
+                // Notify users by push notification
+
+            } catch (exception) {
+                connection.end();
+                response = badRequest;
+                return response;
+            }
+        }
+    } catch (exception) {
+
+    }
+
+    response.body = JSON.stringify(response.body);
+    return response;
 }
