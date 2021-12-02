a03e5b799ada81b45d713da7ce8e17fbc09a0ef6
Jamil Nawaz <jamilnawaz88@gmail.com>
2021-12-02 22:33:53 +0000

    brain dumping logic

--- src/handlers/game/post-game.js
+++ src/handlers/game/post-game.js
@@ -55,13 +55,14 @@ exports.postGameHandler = async(event, context, callback, connection) => {
 
             try {
                 var allMobileNumbers = [];
-                // // const playersObservable = new Observable()
-                const game = of(event.body.game);
+                
+                const game = from(event.body.game);
                 const invitedPlayers = from(event.body.invitedPlayers);
                 const filteredMobile = invitedPlayers.pipe(filter(invite => invite.mobile_number !== '' || invite.mobile_number !== undefined)).subscribe(invite => {
                     allMobileNumbers.push(invite.mobile_number);
                 });
 
+                const allMobileNumbersSqlPortion = 
                 
                 // Determine which users are registered
                 var nonRegisteredUsersSql = "SELECT * FROM User WHERE mobile_number in";
