5db40abc4591e1bfab22c2a87f6c820cb36c06d1
Jamil Nawaz <jamilnawaz88@gmail.com>
2022-01-18 21:56:39 +0000

    SQUAD-56: PUT game details tests added

--- __tests__/unit/handlers/game/put-game.test.js
+++ __tests__/unit/handlers/game/put-game.test.js
@@ -1,7 +1,68 @@
+const mysql = require('mysql');
 const lambda = require('../../../../src/handlers/game/put-game');
 
-describe('Test putGameHandler', () => {
-    it('should update game details', async () => {
-        expect(1).toEqual(1);
+var event, context, callback;
+
+describe('Test putGameHandlerById', () => {
+    it('should not accept the POST http method', async done => {
+        event = {
+            httpMethod: 'POST',
+            id: 'game_id'
+        };
+        
+        const result = await lambda.putGameHandlerById(event, context, callback, mysql);
+        
+        const expectedResult = {
+            "message": "Bad request",
+            "reason": "putGameHandlerById only accepts PUT method, you tried: POST",
+            "statusCode": 400
+        };
+        
+        expect(result).toEqual(expectedResult);
     });
+    
+    it('should not accept the GET http method', async done => {
+        event = {
+            httpMethod: 'GET',
+            id: 'game_id'
+        };
+        
+        const result = await lambda.putGameHandlerById(event, context, callback, mysql);
+        
+        const expectedResult = {
+            "message": "Bad request",
+            "reason": "putGameHandlerById only accepts PUT method, you tried: GET",
+            "statusCode": 400
+        };
+        
+        expect(result).toEqual(expectedResult);
+    });
+    
+    it('should find the game and mark all the invited players as uninvited', async done => {
+        event = {
+            httpMethod: 'PUT',
+            id: 'game_id'
+        };
+        
+        const result = await lambda.putGameHandlerById(event, context, callback, mysql);
+
+        const expectedResult = {
+            statusCode: 201,
+            body: {
+                results: {
+                    game: {
+
+                    },
+                    invitedPlayers: [
+                        {
+                            
+                        }
+                    ]
+                }
+            }
+        }
+
+        expect(result).toEqual(expectedResult);
+    });
+
 });
