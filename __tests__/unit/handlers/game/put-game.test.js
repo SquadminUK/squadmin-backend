f59ada67e23cde57a633e9ea8723189858731484
Jamil Nawaz <jamilnawaz88@gmail.com>
2022-01-20 16:20:55 +0000

    SQUAD-56: added test for updating the game

--- __tests__/unit/handlers/game/put-game.test.js
+++ __tests__/unit/handlers/game/put-game.test.js
@@ -3,6 +3,15 @@ const lambda = require('../../../../src/handlers/game/put-game');
 
 var event, context, callback;
 
+jest.mock('mysql', () => ({
+    state: 'disconnected',
+    createConnection: () => {},
+    connect: jest.fn().mockImplementation((callback) => callback()),
+    format: jest.fn(),
+    query: jest.fn().mockImplementation((query, callback) => callback(null, [])),
+    end: jest.fn()
+}));
+
 describe('Test putGameHandlerById', () => {
     it('should not accept the POST http method', async done => {
         event = {
@@ -44,34 +53,42 @@ describe('Test putGameHandlerById', () => {
         done();
     });
     
-    it('should find the game and mark all the invited players as uninvited', async done => {
+    it('should update game details', async done => {
+
         event = {
             httpMethod: 'PUT',
             pathParameters: {
                 id: 'game_id'
+            },
+            body: {
+                game: {
+                    location: "location",
+                    event_date: "event_date",
+                    is_active: true
+                },
+                invitedPlayers:[]
             }
         };
         
         const result = await lambda.putGameByIdHandler(event, context, callback, mysql);
-
+        
         const expectedResult = {
             statusCode: 201,
             body: {
                 results: {
-                    game: {
-
+                    game: {                        
+                        location: "location",
+                        event_date: "event_date",
+                        is_active: true
                     },
-                    invitedPlayers: [
-                        {
-
-                        }
-                    ]
+                    invitedPlayers: []
                 }
             }
-        }
-
+        };
+        
         expectedResult.body = JSON.stringify(expectedResult.body);
         expect(result).toEqual(expectedResult);
+        done();
     });
 
 });
--- src/handlers/game/put-game.js
+++ src/handlers/game/put-game.js
@@ -6,9 +6,7 @@ exports.putGameByIdHandler = async (event, context, callback, connection) => {
         statusCode: 201,
         body: {
             results: {
-                game: {
-                    
-                },
+                game: {},
                 invitedPlayers: []
             }
         }
@@ -19,6 +17,37 @@ exports.putGameByIdHandler = async (event, context, callback, connection) => {
         message: "Bad request",
         reason: null
     };
+    
+    function badRequest(message) {
+        if (message) {
+            badRequest.reason = message
+        }
+        
+        return badRequest;
+    }
+    
+    var gameId = undefined;
+    
+    async function updateGameDetails() {
+        try {
+            return new Promise((resolve, reject) => {
+                var getGameDetailsSql = "UPDATE OrganisedGame set location = ?, event_date = ?, date_modified = now(), is_active = ? WHERE game_id = ?";
+                var updateGameParams = [event.body.location, event.body.event_date, event.body.is_active, gameId];
+                const formattedGetGameQuery = mysql.format(getGameDetailsSql, updateGameParams);
+                
+                connection.query(formattedGetGameQuery, function (err, results) {
+                    if (err) {
+                        throw new Error('There was an issue with the GetGame SQL query');
+                    }
+                    response.body.results.game = event.body.game;
+                    resolve();
+                    
+                });
+            });
+        } catch (exception) {
+            return badRequest(exception.message);            
+        }
+    }
     
     if (connection === undefined) {
         connection = mysql.createConnection({
@@ -31,24 +60,52 @@ exports.putGameByIdHandler = async (event, context, callback, connection) => {
             multipleStatements: true
         });            
     }
-
-    var gameId = undefined;
+    
     try {
         const { httpMethod } = event;
         gameId = event.pathParameters.id;
         if (httpMethod !== 'PUT') {
             throw new Error(`putGameHandlerById only accepts PUT method, you tried: ${httpMethod}`);
         }
-
+        
         if (gameId === undefined || gameId === '') {
             throw new Error('No game id provided');
         }
+        
+        if (typeof event.body === 'string') {
+            event.body = JSON.parse(event.body);
+        }
+        
     } catch(exception) {
         badRequest.reason = exception.message;
         return badRequest;
     }
+    try {
+        if (connection.state === 'disconnected') {
+            await new Promise((resolve, reject) => {
+                connection.connect(function (err) {
+                    if (err) {
+                        throw new Error('Failed to connect');    
+                    }
+                    resolve();
+                });
+            });
 
+            await new Promise((resolve, reject) => {
+                updateGameDetails().then(() => {
+                    resolve();
+                });
+            });
+        }
+        
+    } catch (exception) {
+        connection.end();
+        response = badRequest;
+        response.reason = exception.message;
+        return response;
+    }
+    
     response.body = JSON.stringify(response.body);
     return response;
-
+    
 }
