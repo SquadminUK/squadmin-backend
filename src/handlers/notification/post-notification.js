11193967ce2b5e30a6b5cb0b64e58db277ff11bc
Jamil Nawaz <jamilnawaz88@gmail.com>
2022-06-01 11:06:29 +0100

    Implementation for post-notification

--- src/handlers/notification/post-notification.js
+++ src/handlers/notification/post-notification.js
@@ -1,3 +1,12 @@
+
 exports.postNotificationHandler = async (event, context, callback) => {
 
+    let response = {
+        statusCode: 200,
+        body: {
+            results: {
+
+            }
+        }
+    }
 }
