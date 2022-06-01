1fe7c84bd2e5a451f1962caf521f1fd82b144d3e
Jamil Nawaz <jnawaz@users.noreply.github.com>
2022-06-01 12:51:08 +0100

    Onesignal notification lambda (#100)
    * SQU-87: Building out OneSignal
    
    * SQU-87: Testing env vars and push notification attempt

--- __tests__/unit/handlers/notification/post-notification.test.js
+++ __tests__/unit/handlers/notification/post-notification.test.js
@@ -1,7 +1,47 @@
+const lambda = require('../../../../src/handlers/notification/post-notification');
 let event, context, callback;
 
 describe('Test postNotificationHandler', () => {
+
+    it('should only accept a POST request', async done => {
+        event = {
+            httpMethod: 'PUT'
+        }
+
+        const result = await lambda.postNotificationHandler(event, context, callback);
+
+        const expectedResult = {
+            statusCode: 400,
+            body: {
+                message: "Unsupported operation"
+            }
+        }
+
+        expectedResult.body = JSON.stringify(expectedResult.body);
+        expect(result).toEqual(expectedResult);
+        done();
+
+    });
+
    it('should post a notification to OneSignal', async done => {
+
+       event = {
+           httpMethod: 'POST'
+       }
+
+       const result = await lambda.postNotificationHandler(event, context, callback);
+
+       const expectedResult = {
+           statusCode: 200,
+           body: {
+               results: {
+                   message: "Successful request made"
+               }
+           }
+       }
+
+       expectedResult.body = JSON.stringify(expectedResult.body);
+       expect(result).toEqual(expectedResult);
 
        done();
    });
--- src/handlers/notification/post-notification.js
+++ src/handlers/notification/post-notification.js
@@ -1,12 +1,71 @@
+const OneSignal = require('@onesignal/node-onesignal');
 
 exports.postNotificationHandler = async (event, context, callback) => {
+
+    const app_key_provider = {
+        getToken() {
+            return process.env.ONE_SIGNAL_API_KEY;
+        }
+    };
+    const user_key_provider = {
+        getToken() {
+            return process.env.ONE_SIGNAL_AUTH_KEY;
+        }
+    };
+
+    console.log(`OS APIKEY ${process.env.ONE_SIGNAL_API_KEY}`);
+    console.log(`OS AUTH KEY ${process.env.ONE_SIGNAL_AUTH_KEY}`);
+
+    const configuration = OneSignal.createConfiguration({
+        authMethods: {
+            user_key: {
+                tokenProvider: user_key_provider
+            },
+            app_key: {
+                tokenProvider: app_key_provider
+            }
+        }
+    });
 
     let response = {
         statusCode: 200,
         body: {
             results: {
-
+                message: "Successful request made"
             }
         }
+    };
+
+    const unsupportedOperation = {
+        statusCode: 400,
+        body:{
+            message: "Unsupported operation"
+        }
+    };
+    try {
+        const { httpMethod } = event;
+        if (httpMethod !== 'POST') {
+            throw new Error(`getGamesHandler only accepts POST method, you tried: ${httpMethod}`);
+        }
+    } catch (exception) {
+        response = unsupportedOperation;
     }
+
+    const client = new OneSignal.DefaultApi(configuration);
+    const notification = new OneSignal.Notification();
+    notification.app_id = 'c8937e94-0548-48d4-b757-4c859cf6d722';
+    notification.channel_for_external_user_ids = "push"
+    notification.contents = {
+        en: "This is a push notification from lambda"
+    };
+
+    // required for Huawei
+    notification.headings = {
+        en: ""
+    };
+    notification.include_external_user_ids = ['892d7e81-a90f-48e4-85b4-11d0d80c51dc'];
+    await client.createNotification(notification);
+
+    response.body = JSON.stringify(response.body);
+    return response;
 }
