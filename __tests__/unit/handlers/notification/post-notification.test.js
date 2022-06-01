af49b703f470c670a6290510e6ba85d74c6d5d9e
Jamil Nawaz <jamilnawaz88@gmail.com>
2022-06-01 12:48:33 +0100

    SQU-87: Testing env vars and push notification attempt

--- __tests__/unit/handlers/notification/post-notification.test.js
+++ __tests__/unit/handlers/notification/post-notification.test.js
@@ -2,7 +2,32 @@ const lambda = require('../../../../src/handlers/notification/post-notification'
 let event, context, callback;
 
 describe('Test postNotificationHandler', () => {
+
+    it('should only accept a POST request', async done => {
+        event = {
+            httpMethod: 'PUT'
+        }
+
+        const result = await lambda.postNotificationHandler(event, context, callback);
+
+        const expectedResult = {
+            statusCode: 400,
+            body: {
+                message: "Unsupported operation"
+            }
+        }
+
+        expectedResult.body = JSON.stringify(expectedResult.body);
+        expect(result).toEqual(expectedResult);
+        done();
+
+    });
+
    it('should post a notification to OneSignal', async done => {
+
+       event = {
+           httpMethod: 'POST'
+       }
 
        const result = await lambda.postNotificationHandler(event, context, callback);
 
