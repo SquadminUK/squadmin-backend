4ce0d353a3c6cbfe451c9d9784cff5e497702868
Jamil Nawaz <jamilnawaz88@gmail.com>
2022-06-01 15:27:32 +0100

    SQU-87: Working on unit test and mocking of createFunction

--- __mocks__/@onesignal/node-onesignal.js
+++ __mocks__/@onesignal/node-onesignal.js
@@ -1,6 +1,5 @@
 module.exports = {
     createConfiguration: jest.fn(),
     DefaultApi: jest.fn(),
-    Notification: jest.fn(),
-    // createNotification: jest.fn()
+    Notification: jest.fn()
 }
