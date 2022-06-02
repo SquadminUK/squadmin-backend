e6df116d4c367e52f3318492879205ac9e33c79a
Jamil Nawaz <jamilnawaz88@gmail.com>
2022-06-03 00:57:23 +0100

    SQU-87: Two tests passing

--- src/handlers/notification/post-notification.js
+++ src/handlers/notification/post-notification.js
@@ -38,32 +38,75 @@ exports.postNotificationHandler = async (event, context, callback) => {
 
     const unsupportedOperation = {
         statusCode: 400,
-        body:{
+        body: {
             message: "Unsupported operation"
         }
     };
+
+    const badRequest = {
+        statusCode: 400,
+        body: {
+            message: "Bad request"
+        }
+    }
+
     try {
-        const { httpMethod } = event;
+        const {httpMethod} = event;
         if (httpMethod !== 'POST') {
             throw new Error(`getGamesHandler only accepts POST method, you tried: ${httpMethod}`);
         }
+
+        event.body = JSON.parse(event.body);
+
     } catch (exception) {
         response = unsupportedOperation;
+        response.body = JSON.stringify(response.body);
+        return response;
     }
 
     const client = new OneSignal.DefaultApi(configuration);
     const notification = new OneSignal.Notification();
     notification.app_id = 'c8937e94-0548-48d4-b757-4c859cf6d722';
     notification.channel_for_external_user_ids = "push"
-    notification.contents = {
-        en: "This is a push notification from lambda"
-    };
 
-    // required for Huawei
+    let notificationTitle = '';
+    let notificationSubtitle = '';
+    let externalUserIds = [];
+
+
+    switch (event.body.notification_type) {
+        case "invitation_response": {
+            notificationTitle = 'Invitation response!';
+            let canOrCant = event.body.response.can_play ? "can" : "can't";
+            notificationSubtitle = `${event.body.response.player_name} has confirmed they ${canOrCant} play`;
+            externalUserIds.push(event.body.inviting_player.inviting_player_id);
+            break;
+        }
+        case "organised_game": {
+            notificationTitle = `${event.body.inviting_player} has invited you to a game`;
+            notificationSubtitle = "Let them know if you can make it";
+            if (event.body.invited_players !== undefined) {
+                event.body.invited_players.forEach(function (value, index, array) {
+                    externalUserIds.push(value);
+                });
+            }
+            break;
+        }
+    }
+
+    notification.contents = {
+        en: notificationTitle
+    }
     notification.headings = {
-        en: ""
+        en: notificationTitle
     };
-    notification.include_external_user_ids = ['892d7e81-a90f-48e4-85b4-11d0d80c51dc'];
+    notification.subtitle = {
+        en: notificationSubtitle
+    };
+    notification.include_external_user_ids = externalUserIds;
+    // notification.include_external_user_ids = ['892d7e81-a90f-48e4-85b4-11d0d80c51dc'];
+
+
     await client.createNotification(notification);
 
     response.body = JSON.stringify(response.body);
