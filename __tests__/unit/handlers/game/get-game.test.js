d5b31d94adccd1499b8d2cee3ca99ad5489febb5
Jamil Nawaz <jnawaz@users.noreply.github.com>
2022-06-29 11:20:36 +0100

    SQU-97 updating get game response (#104)
    * SQU-97: Adding user details to response
    
    * SQU-97: Added user details to get game details response
    
    * Updated gitignore
    
    * Updated gitignore

--- __tests__/unit/handlers/game/get-game.test.js
+++ __tests__/unit/handlers/game/get-game.test.js
@@ -94,6 +94,67 @@ describe('Test getGameHandler', () => {
     });
     
     it('should retrieve game details by id', async done => {
+
+        mysql.query = jest.fn().mockImplementation((query, callback) => callback(null, [
+            {
+                Game: {
+                    game_id: 'game_id',
+                    venue: 'venue',
+                    location: 'location',
+                    date_created: 'date_created',
+                    date_modified: 'date_modified',
+                    organising_player: 'organising_player',
+                },
+                Invitation: {
+                    organised_game_id: 'organised_game_id',
+                    response_id: 'response_id_1',
+                    date_responded: 'date_responded_1',
+                    can_play: null,
+                    date_modified: 'date_modified_1',
+                    user_id: 'user_id_1'
+                },
+                UserTable: {
+                    user_id: 'user_id_1',
+                    full_name: 'full_name_1',
+                    email_address: 'email_address_1',
+                    mobile_number: 'mobile_number_1',
+                    username: 'username_1',
+                    date_of_birth: 'date_of_birth_1',
+                    date_created: 'date_created_1',
+                    date_modified: 'date_modified_1',
+                    signed_up_via_social: true
+                }
+            },
+            {  Game: {
+                    game_id: 'game_id',
+                    venue: 'venue',
+                    location: 'location',
+                    date_created: 'date_created',
+                    date_modified: 'date_modified',
+                    organising_player: 'organising_player',
+                },
+                Invitation: {
+                    organised_game_id: 'organised_game_id',
+                    response_id: 'response_id_2',
+                    date_responded: 'date_responded_2',
+                    can_play: true,
+                    date_modified: 'date_modified_2',
+                    user_id: 'user_id_2'
+                },
+                UserTable: {
+                    user_id: 'user_id_2',
+                    full_name: 'full_name_2',
+                    email_address: 'email_address_2',
+                    mobile_number: 'mobile_number_2',
+                    username: 'username_2',
+                    date_of_birth: 'date_of_birth_2',
+                    date_created: 'date_created_2',
+                    date_modified: 'date_modified_2',
+                    signed_up_via_social: true
+                }
+            }
+        ]));
+
         event = {
             httpMethod: 'GET',
             pathParameters: {
@@ -118,11 +179,41 @@ describe('Test getGameHandler', () => {
                     invitedPlayers: [
                         {
                             organised_game_id: 'organised_game_id',
-                            response_id: 'response_id',
-                            date_responded: 'date_responded',
-                            can_play: 'can_play',
-                            date_modified: 'date_modified',
-                            user_id: 'user_id'
+                            response_id: 'response_id_1',
+                            date_responded: 'date_responded_1',
+                            can_play: null,
+                            date_modified: 'date_modified_1',
+                            user_id: 'user_id_1',
+                            user_details: {
+                                user_id: 'user_id_1',
+                                full_name: 'full_name_1',
+                                email_address: 'email_address_1',
+                                mobile_number: 'mobile_number_1',
+                                username: 'username_1',
+                                date_of_birth: 'date_of_birth_1',
+                                date_created: 'date_created_1',
+                                date_modified: 'date_modified_1',
+                                signed_up_via_social: true
+                            }
+                        },
+                        {
+                            organised_game_id: 'organised_game_id',
+                            response_id: 'response_id_2',
+                            date_responded: 'date_responded_2',
+                            can_play: true,
+                            date_modified: 'date_modified_2',
+                            user_id: 'user_id_2',
+                            user_details: {
+                                user_id: 'user_id_2',
+                                full_name: 'full_name_2',
+                                email_address: 'email_address_2',
+                                mobile_number: 'mobile_number_2',
+                                username: 'username_2',
+                                date_of_birth: 'date_of_birth_2',
+                                date_created: 'date_created_2',
+                                date_modified: 'date_modified_2',
+                                signed_up_via_social: true
+                            }
                         }
                     ]
                 }
--- src/handlers/game/get-game.js
+++ src/handlers/game/get-game.js
@@ -1,27 +1,26 @@
 var mysql = require('mysql');
-const { from } = require('rxjs');
-const { filter, first } = require('rxjs/operators');
+const {from} = require('rxjs');
+const {filter, first} = require('rxjs/operators');
 
 exports.getGameByIdHandler = async (event, context, callback, connection) => {
-    
+
     var response = {
         statusCode: 200,
         body: {
             results: {
-                game: {
-                },
+                game: {},
                 invitedPlayers: []
             }
         }
     }
-    
+
     var badRequest = {
         statusCode: 400,
         message: "Bad request",
         reason: null
     };
-    
-    if (connection === undefined) { 
+
+    if (connection === undefined) {
         connection = mysql.createConnection({
             connectionLimit: 10,
             host: process.env.RDS_HOSTNAME,
@@ -32,23 +31,23 @@ exports.getGameByIdHandler = async (event, context, callback, connection) => {
             multipleStatements: true
         });
     }
-    
+
     var gameId = undefined;
     try {
-        const { httpMethod } = event;
+        const {httpMethod} = event;
         gameId = event.pathParameters.id;
         if (httpMethod !== 'GET') {
             throw new Error(`getGameHandlerById only accepts GET method, you tried: ${httpMethod}`);
         }
-        
+
         if (gameId === undefined || gameId === '') {
             throw new Error('No game id provided');
         }
-    } catch(exception) {
+    } catch (exception) {
         badRequest.reason = exception.message;
         return badRequest;
     }
-    
+
     try {
         if (connection.state === 'disconnected') {
             await new Promise((resolve, reject) => {
@@ -60,26 +59,41 @@ exports.getGameByIdHandler = async (event, context, callback, connection) => {
                     resolve();
                 });
             });
-            
+
             try {
-                var getGameDetailsSql = "SELECT * FROM OrganisedGame Game INNER JOIN GameInvitation Invitation ON Game.game_id = Invitation.organised_game_id WHERE Game.game_id = ?";
+                var getGameDetailsSql = `SELECT * FROM OrganisedGame Game 
+                                         INNER JOIN GameInvitation Invitation 
+                                         ON Game.game_id = Invitation.organised_game_id
+                                         INNER JOIN User UserTable ON Invitation.user_id = UserTable.user_id 
+                                         WHERE Game.game_id = ?`;
                 var gameParams = gameId;
                 var formattedGetGameQuery = mysql.format(getGameDetailsSql, gameParams);
-                
+
                 var options = {sql: formattedGetGameQuery, nestTables: true};
-                
+
                 await new Promise((resolve, reject) => {
-                    connection.query(options, function(err, results) {
+                    connection.query(options, function (err, results) {
                         if (err) {
                             connection.end();
                             throw new Error(`There was an issue with the SQL statement: ${formattedGetGameQuery}`);
                         }
-                        
+
                         const retrievedDetails = from(results);
-                        
+
                         const game = retrievedDetails.pipe(first()).subscribe((game) => response.body.results.game = game.Game);
-                        const invites = retrievedDetails.pipe().subscribe((invitation) => { response.body.results.invitedPlayers.push(invitation.Invitation); });
-                        
+                        const invites = retrievedDetails.pipe().subscribe((invitation) => {
+                            response.body.results.invitedPlayers.push(invitation.Invitation);
+                        });
+                        const users = retrievedDetails.pipe().subscribe((user) => {
+                           var invitedPlayers = response.body.results.invitedPlayers;
+                           invitedPlayers.forEach(function (player, index, array) {
+                               if (player.user_id === user.UserTable.user_id) {
+                                   player.user_details = user.UserTable;
+                               }
+                           });
+                           response.body.results.invitedPlayers = invitedPlayers;
+                        });
+
                         resolve();
                     });
                 });
@@ -89,14 +103,14 @@ exports.getGameByIdHandler = async (event, context, callback, connection) => {
                 response.reason = exception.message;
                 return response;
             }
-            
+
         }
     } catch (exception) {
         connection.end();
         badRequest.reason = exception.message;
         return badRequest;
     }
-    
+
     connection.end();
     response.body = JSON.stringify(response.body);
     return response;
