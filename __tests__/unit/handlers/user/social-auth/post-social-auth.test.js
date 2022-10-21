9ae3a901ca4c56970147baa281d7cabaee0006a0
Jamil Nawaz <jnawaz@users.noreply.github.com>
2022-10-21 18:43:26 +0100

    Social Auth login. (#105)

---
+++ __tests__/unit/handlers/user/social-auth/post-social-auth.test.js
@@ -0,0 +1,161 @@
+const mysql = require('mysql');
+const lambda = require('../../../../../src/handlers/user/social-auth/post-social-auth');
+
+let event, context, callback;
+jest.mock('mysql', () => ({
+  state: 'disconnected',
+  createConnection: () => {},
+  connect: jest.fn().mockImplementation((callback) => { callback() }),
+  format: jest.fn(),
+  query: jest.fn().mockImplementation((query, callback) => callback(null, [])),
+  end: jest.fn()
+}));
+
+describe('Test postSocialAuthHandler', () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
+  it('should not acccept PUT method', async done => {
+    event = {
+      httpMethod: 'PUT',
+      pathParameters: {
+        user_id: 'user_id'
+      }
+    };
+
+    event = JSON.stringify(event);
+
+    const result = await lambda.postSocialAuthHandler(event, context, callback, mysql);
+
+    const expectedResult = {
+      statusCode: 400,
+      message: 'Bad request',
+      reason: 'postSocialAuthHandler only accepts POST method, you tried: PUT'
+    };
+
+    expect(result).toEqual(JSON.stringify(expectedResult));
+    done();
+  });
+
+  it('should not acccept GET method', async done => {
+    event = {
+      httpMethod: 'GET',
+      pathParameters: {
+        user_id: 'user_id'
+      }
+    };
+
+    event = JSON.stringify(event);
+
+    const result = await lambda.postSocialAuthHandler(event, context, callback, mysql);
+
+    const expectedResult = {
+      statusCode: 400,
+      message: 'Bad request',
+      reason: 'postSocialAuthHandler only accepts POST method, you tried: GET'
+    };
+
+    expect(result).toEqual(JSON.stringify(expectedResult));
+    done();
+  });
+
+  it('should return an error when no email address is provided', async done => {
+
+    event = {
+      httpMethod: 'POST',
+      pathParameters: {
+        user_id: 'user_id'
+      },
+      body: {
+        email_address: ''
+      }
+    };
+    event = JSON.stringify(event);
+
+    const result = await lambda.postSocialAuthHandler(event, context, callback, mysql);
+
+    const expectedResult = {
+      statusCode: 400,
+      message: 'Bad request',
+      reason: 'User email not provided'
+    };
+
+    expect(result).toEqual(JSON.stringify(expectedResult));
+    done();
+  });
+
+  it('should return an error when an invalid email address is provided', async done => {
+
+    event = {
+      httpMethod: 'POST',
+      pathParameters: {
+        user_id: 'user_id'
+      },
+      body: {
+        email_address: 'x.com'
+      }
+    };
+    event = JSON.stringify(event);
+
+    const result = await lambda.postSocialAuthHandler(event, context, callback, mysql);
+
+    const expectedResult = {
+      statusCode: 400,
+      message: 'Bad request',
+      reason: 'Invalid email address'
+    };
+
+    expect(result).toEqual(JSON.stringify(expectedResult));
+    done();
+  });
+
+  it('should return the user details when user details are already registered', async done => {
+    event = {
+      httpMethod: 'POST',
+      pathParameters: {
+        user_id: 'user_id'
+      },
+      body: {
+        email_address: 'jamilnawaz88@gmail.com'
+      }
+    };
+    event = JSON.stringify(event);
+    mysql.query = jest.fn()
+      .mockImplementationOnce((query, callback) => callback(null, [
+        {
+          user_id: 'user_id',
+          full_name: 'full_name',
+          email_address: 'email_address',
+          mobile_number: 'mobile_number',
+          username: 'username',
+          date_of_birth: 'date_of_birth',
+          date_created: 'date_created',
+          date_modified: 'date_modified',
+          signed_up_via_social: true,
+          has_registered_via_client: true
+        }
+      ]))
+
+    const result = await lambda.postSocialAuthHandler(event, context, callback, mysql);
+
+    const expectedResult = {
+      statusCode: 200,
+      body: {
+        user: {
+          user_id: 'user_id',
+          email_address: 'email_address',
+          date_created: 'date_created',
+          date_modified: 'date_modified',
+          signed_up_via_social: true,
+          has_registered_via_client: true
+        }
+      }
+
+    };
+
+    expect(result).toEqual(JSON.stringify(expectedResult));
+    done();
+  });
+
+});
--- src/handlers/user/get-user.js
+++ src/handlers/user/get-user.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 exports.getUserHandler = async (event, context, callback, connection) => {
-    var response = {
+    let response = {
         statusCode: 200,
         body: {
             results: {
@@ -17,22 +17,22 @@ exports.getUserHandler = async (event, context, callback, connection) => {
             }
         }
     }
-    
+
     var badRequest = {
         statusCode: 400,
         message: 'Bad request',
         reason: null
     };
-    
+
     var noUserFound = {
         statusCode: 200,
         body: {
             results: [],
             message: ''
-        } 
+        }
     };
-    
-    if (connection === undefined) { 
+
+    if (connection === undefined) {
         connection = mysql.createConnection({
             connectionLimit: 10,
             host: process.env.RDS_HOSTNAME,
@@ -42,18 +42,18 @@ exports.getUserHandler = async (event, context, callback, connection) => {
             database: process.env.RDS_DATABASE
         });
     }
-    
+
     try {
         if (event.body) {
             event.body = JSON.parse(event.body);
         }
         const { httpMethod } = event;
         const userId = event.pathParameters.user_id;
-        
-        if (httpMethod != 'GET') {
+
+        if (httpMethod !=='GET') {
             throw new Error(`getUserHandler only accepts GET method, you tried: ${httpMethod}`);
         }
-        
+
         if (userId === undefined || userId === '') {
             throw new Error('No user id provided');
         }
@@ -61,7 +61,7 @@ exports.getUserHandler = async (event, context, callback, connection) => {
         badRequest.reason = exception.message;
         return badRequest;
     }
-    
+
     try {
         if (connection.state === 'disconnected') {
             await new Promise((resolve, reject) => {
@@ -73,19 +73,19 @@ exports.getUserHandler = async (event, context, callback, connection) => {
                     resolve();
                 })
             });
-            
+
             try {
                 var getUserSql = "SELECT * FROM User WHERE user_id = ?";
                 var userId = event.pathParameters.user_id;
                 var formattedGetUserQuery = mysql.format(getUserSql, userId);
-                
+
                 await new Promise((resolve, reject) => {
                     connection.query(formattedGetUserQuery, function(err, results) {
                         if (err) {
                             new Error('There was an issue with the SQL statement');
                             reject();
                         }
-                        
+
                         var user;
                         if (results.length > 0) {
                             user = results[0];
@@ -93,9 +93,9 @@ exports.getUserHandler = async (event, context, callback, connection) => {
                         } else {
                             throw new Error('No users found');
                         }
-                        
+
                         if (user) {
-                            
+
                             response.body.results.user_id = user.user_id;
                             response.body.results.full_name = user.full_name;
                             response.body.results.email_address = user.email_address;
@@ -105,20 +105,20 @@ exports.getUserHandler = async (event, context, callback, connection) => {
                             response.body.results.date_created = user.date_created;
                             response.body.results.date_modified = user.date_modified;
                             response.body.results.signed_up_via_social = user.signed_up_via_social;
-                            
+
                         }
                         connection.end();
                         resolve();
                     });
-                    
+
                 });
-                
+
             } catch (exception) {
                 connection.end();
                 badRequest.reason = exception.message;
                 return badRequest;
             }
-            
+
         }
     } catch (exception) {
         connection.end();
@@ -126,7 +126,7 @@ exports.getUserHandler = async (event, context, callback, connection) => {
         response.reason = exception.message;
         return response;
     }
-    
+
     response.body = JSON.stringify(response.body);
     return response;
 }
