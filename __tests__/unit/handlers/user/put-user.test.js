419f81a036c2f3485846def56c21adc2c28e7bce
Jamil Nawaz <jamilnawaz88@gmail.com>
2022-11-19 12:17:23 +0000

    Update user and return full user details.

--- __tests__/unit/handlers/user/put-user.test.js
+++ __tests__/unit/handlers/user/put-user.test.js
@@ -25,9 +25,9 @@ jest.mock('mysql', () => ({
 }));
 
 describe('Test putUserHandler', () => {
-    
+
     beforeEach( () => jest.resetModules() );
-    
+
     it('should not accept GET method', async done => {
         event = {
             httpMethod: 'GET',
@@ -35,20 +35,20 @@ describe('Test putUserHandler', () => {
                 user_id: 'user_id'
             }
         };
-        
+
         const result = await lambda.putUserHandler(event, context, callback, mysql);
-        
+
         const expectedResult = {
             statusCode: 400,
             message: 'Bad request',
             reason: 'putUserHandler only accepts PUT method, you tried: GET'
         }
-        
+
         expect(result).toEqual(expectedResult);
-        
+
         done();
     });
-    
+
     it('should not accept POST method', async done => {
         event = {
             httpMethod: 'POST',
@@ -56,44 +56,44 @@ describe('Test putUserHandler', () => {
                 user_id: 'user_id'
             }
         };
-        
+
         const result = await lambda.putUserHandler(event, context, callback, mysql);
-        
+
         const expectedResult = {
             statusCode: 400,
             message: 'Bad request',
             reason: 'putUserHandler only accepts PUT method, you tried: POST'
         }
-        
+
         expect(result).toEqual(expectedResult);
-        
+
         done();
     });
-    
+
     it('should fail with a suitable error when no user_id provided', async done => {
-        
+
         event = {
             httpMethod: 'PUT',
             pathParameters: {
                 user_id: ''
             }
         };
-        
+
         const result = await lambda.putUserHandler(event, context, callback, mysql);
-        
+
         const expectedResult = {
             "message": "Bad request",
             "reason": "No user id provided",
             "statusCode": 400
         };
-        
+
         expect(result).toEqual(expectedResult);
-        
+
         done();
     });
-    
+
     it('should successfully retrieve user details', async done => {
-        
+
         mysql.connect = jest.fn().mockImplementation((callback) => callback());
         mysql.query = jest.fn().mockImplementation((query, callback) => callback(null, [
             {
@@ -126,11 +126,11 @@ describe('Test putUserHandler', () => {
                     signed_up_via_social: true
             }
         };
-        
+
         event.body = JSON.stringify(event.body);
 
         const result = await lambda.putUserHandler(event, context, callback, mysql);
-        
+
         const expectedResult = {
             statusCode: 200,
             body: {
@@ -143,15 +143,16 @@ describe('Test putUserHandler', () => {
                     date_of_birth: 'date_of_birth',
                     date_created: 'date_created',
                     date_modified: 'date_modified',
-                    signed_up_via_social: true
+                    signed_up_via_social: true,
+                    has_registered_via_client: false
                 }
             }
         }
-        
+
         expectedResult.body = JSON.stringify(expectedResult.body);
 
         expect(result).toEqual(expectedResult);
         done();
     });
-    
+
 });
--- src/handlers/user/put-user.js
+++ src/handlers/user/put-user.js
@@ -1,112 +1,147 @@
 const mysql = require('mysql');
 
 exports.putUserHandler = async (event, context, callback, connection) => {
-    var response = {
-        statusCode: 200,
-        body: {
-            results: {
-                user_id: '',
-                full_name: '',
-                email_address: '',
-                mobile_number: '',
-                username: '',
-                date_of_birth: '',
-                date_created: '',
-                date_modified: '',
-                signed_up_via_social: ''
-            }
-        }
+  var response = {
+    statusCode: 200,
+    body: {
+      results: {
+        user_id: '',
+        full_name: '',
+        email_address: '',
+        mobile_number: '',
+        username: '',
+        date_of_birth: '',
+        date_created: '',
+        date_modified: '',
+        signed_up_via_social: ''
+      }
     }
-    
-    var badRequest = {
-        statusCode: 400,
-        message: 'Bad request',
-        reason: null
-    };
-    
-    var noUserFound = {
-        statusCode: 200,
-        body: {
-            results: [],
-            message: ''
-        } 
-    };
-    
-    if (connection === undefined) { 
-        connection = mysql.createConnection({
-            connectionLimit: 10,
-            host: process.env.RDS_HOSTNAME,
-            user: process.env.RDS_USERNAME,
-            password: process.env.RDS_PASSWORD,
-            port: process.env.RDS_PORT,
-            database: process.env.RDS_DATABASE
-        });
+  }
+
+  var badRequest = {
+    statusCode: 400,
+    message: 'Bad request',
+    reason: null
+  };
+
+  var noUserFound = {
+    statusCode: 200,
+    body: {
+      results: [],
+      message: ''
     }
-    
-    var userId = undefined;
+  };
+
+  async function retrieveUserDetails(userId) {
     try {
-        if (event.body) {
-            event.body = JSON.parse(event.body);
-        }
-        const { httpMethod } = event;
-        userId = event.pathParameters.user_id;
-        
-        if (httpMethod != 'PUT') {
-            throw new Error(`putUserHandler only accepts PUT method, you tried: ${httpMethod}`);
-        }
-        
-        if (userId === undefined || userId === '') {
-            throw new Error('No user id provided');
-        }
-    } catch (exception) {
+      return await new Promise((resolve, reject) => {
+        let getUserQuery = `SELECT * FROM User WHERE user_id = ?`;
+        let formattedQuery = mysql.format(getUserQuery, userId);
+        connection.query(formattedQuery, function (err, results) {
+          if (err) {
+            throw new Error("There was an issue with retrieving user details");
+          }
+
+          if (results.length > 0) {
+            let user = results[0];
+            let responseBody = response.body.results;
+            responseBody.user_id = user['user_id'];
+            responseBody.full_name = user['full_name'];
+            responseBody.email_address = user['email_address'];
+            responseBody.mobile_number = user['mobile_number'];
+            responseBody.username = user['username'];
+            responseBody.date_of_birth = user['date_of_birth'];
+            responseBody.date_created = user['date_created'];
+            responseBody.date_modified = user['date_modified'];
+            responseBody.password = user['password'];
+            responseBody.signed_up_via_social = Boolean(user['signed_up_via_social']);
+            responseBody.has_registered_via_client = Boolean(user['has_registered_via_client']);
+            response.body.results = responseBody;
+            resolve();
+          }
+        });
+      });
+    } catch (e) {
+      connection.end();
+      badRequest.reason = e.message;
+      return badRequest;
+    }
+  }
+
+  if (connection === undefined) {
+    connection = mysql.createConnection({
+      connectionLimit: 10,
+      host: process.env.RDS_HOSTNAME,
+      user: process.env.RDS_USERNAME,
+      password: process.env.RDS_PASSWORD,
+      port: process.env.RDS_PORT,
+      database: process.env.RDS_DATABASE
+    });
+  }
+
+  var userId = undefined;
+  try {
+    if (event.body) {
+      event.body = JSON.parse(event.body);
+    }
+    const {httpMethod} = event;
+    userId = event.pathParameters.user_id;
+
+    if (httpMethod != 'PUT') {
+      throw new Error(`putUserHandler only accepts PUT method, you tried: ${httpMethod}`);
+    }
+
+    if (userId === undefined || userId === '') {
+      throw new Error('No user id provided');
+    }
+  } catch (exception) {
+    badRequest.reason = exception.message;
+    return badRequest;
+  }
+
+  try {
+    if (connection.state === 'disconnected') {
+      await new Promise((resolve, reject) => {
+        connection.connect(function (err) {
+          if (err) {
+            response = badRequest;
+            reject('Failed to connect');
+          }
+          resolve();
+        })
+      });
+
+      try {
+        var putUserSql = "UPDATE User SET full_name = COALESCE(?, NULL), email_address = COALESCE(?, NULL), mobile_number = COALESCE(?, NULL), date_of_birth = COALESCE(?, NULL), date_modified = now() WHERE user_id = ?";
+        var userId = [event.body.full_name, event.body.email_address, event.body.mobile_number, event.body.date_of_birth, userId];
+        var formattedUpdateUserQuery = mysql.format(putUserSql, userId);
+        console.log(formattedUpdateUserQuery);
+
+        var updateUserQuery = await new Promise((resolve, reject) => {
+          connection.query(formattedUpdateUserQuery, function (err, results) {
+            if (err) {
+              new Error('There was an issue with the update user SQL statement');
+              reject('There was an issue with the update user SQL statement');
+            }
+            resolve();
+          });
+        }).then(() => {
+          retrieveUserDetails(userId);
+        });
+      } catch (exception) {
+        connection.end();
         badRequest.reason = exception.message;
         return badRequest;
+      }
+
     }
-    
-    try {
-        if (connection.state === 'disconnected') {
-            await new Promise((resolve, reject) => {
-                connection.connect(function (err) {
-                    if (err) {
-                        response = badRequest;
-                        reject('Failed to connect');
-                    }
-                    resolve();
-                })
-            });
-            
-            try {
-                var putUserSql = "UPDATE User SET full_name = ?, email_address = ?, mobile_number = ?, date_of_birth = ?, date_modified = now() WHERE user_id = ?";
-                var userId = [event.body.full_name, event.body.email_address, event.body.mobile_number, event.body.date_of_birth, userId];
-                var formattedUpdateUserQuery = mysql.format(putUserSql, userId);
-                
-                var updateUserQuery = await new Promise((resolve, reject) => {
-                    connection.query(formattedUpdateUserQuery, function (err, results) {
-                        if (err) {
-                            new Error('There was an issue with the update user SQL statement');
-                            reject();
-                        }
-                        
-                        response.body.results = event.body;
-                        connection.end();
-                        resolve();
-                    });
-                });
-            } 
-            catch (exception) {
-                connection.end();
-                badRequest.reason = exception.message;
-                return badRequest;
-            }
-            
-        }
-    } catch (exception) {
-        connection.end();
-        response = badRequest;
-        response.reason = exception.message;
-        return response;
-    }
-    
-    response.body = JSON.stringify(response.body);
+  } catch (exception) {
+    connection.end();
+    response = badRequest;
+    response.reason = exception.message;
     return response;
+  }
+
+  response.body = JSON.stringify(response.body);
+  return response;
 }
